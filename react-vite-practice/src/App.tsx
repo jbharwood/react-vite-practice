6b0730ea7b584dbb87981768f2080c1e0fd35ba6
Joseph Harwood <josephblakeharwood@gmail.com>
2025-02-27 14:03:41 -0500

    added routing and page navigation

--- react-vite-practice/src/App.tsx
+++ react-vite-practice/src/App.tsx
@@ -1,57 +1,15 @@
-import { useMarketplaceStore } from "./stores/useMarketplaceStore";
 import "./App.css";
-import { useEffect, useState } from "react";
-import SearchBar from "./components/SearchBar";
-import ItemList from "./components/ItemList";
-// import axios from "axios";
-
-interface Item {
-  id: number;
-  name: string;
-  price: string;
-  image: string;
-}
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  const { fetchItems } = useMarketplaceStore();
-  const { items, loading } = useMarketplaceStore();
-  const [searchQuery, setSearchQuery] = useState("");
-
-  useEffect(() => {
-    fetchItems();
-  }, [fetchItems]);
-
-  // useEffect(() => {
-  //   async function getItems() {
-  //     const items = await axios.get(
-  //       "https://jsonplaceholder.typicode.com/posts"
-  //     );
-  //     console.log(items.data);
-  //     return items.data;
-  //   }
-
-  //   getItems();
-  // }, []);
-
-  // useEffect(() => {
-  //   async function getItems() {
-  //     const resp = await fetch("https://jsonplaceholder.typicode.com/posts");
-  //     const data = await resp.json();
-  //     console.log(data);
-  //     return data;
-  //   }
-
-  //   getItems();
-  // }, []);
-
-  if (loading) {
-    return <div>loading...</div>;
-  }
-
   return (
     <main className="app-container">
-      <SearchBar setSearchQuery={setSearchQuery} searchQuery={searchQuery} />
-      <ItemList items={items} searchQuery={searchQuery} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </main>
   );
 }
---
+++ react-vite-practice/src/components/Home.tsx
@@ -0,0 +1,51 @@
+import { useMarketplaceStore } from "../stores/useMarketplaceStore";
+import { useEffect, useState } from "react";
+import SearchBar from "./SearchBar";
+import ItemList from "./ItemList";
+import NavBar from "./NavBar";
+// import axios from "axios";
+
+export default function Home() {
+  const { fetchItems } = useMarketplaceStore();
+  const { items, loading } = useMarketplaceStore();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    fetchItems();
+  }, [fetchItems]);
+
+  // useEffect(() => {
+  //   async function getItems() {
+  //     const items = await axios.get(
+  //       "https://jsonplaceholder.typicode.com/posts"
+  //     );
+  //     console.log(items.data);
+  //     return items.data;
+  //   }
+
+  //   getItems();
+  // }, []);
+
+  // useEffect(() => {
+  //   async function getItems() {
+  //     const resp = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //     const data = await resp.json();
+  //     console.log(data);
+  //     return data;
+  //   }
+
+  //   getItems();
+  // }, []);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      <NavBar />
+      <SearchBar setSearchQuery={setSearchQuery} searchQuery={searchQuery} />
+      <ItemList items={items} searchQuery={searchQuery} />
+    </div>
+  );
+}
